456d969f47df4b6bdfbf335f8a8945a07427f445
jsanzucla <jsanz@mednet.ucla.edu>
2018-02-07 10:11:30 -0800

    optimize joins in query
    simplify pull by reducing the number of fields pulled which also leaves out some joins

--- 4-Finalize Encounters table.sql
+++ 4-Finalize Encounters table.sql
@@ -18,8 +18,6 @@
 --          2534	Scanned Document No Visit
 --            40	Wait List 
 --------------------------------------------------------------------------------
---DROP TABLE XDR_WHERRY_preg_ENC PURGE;
---CREATE TABLE XDR_WHERRY_preg_ENC AS
 INSERT INTO XDR_WHERRY_preg_ENC
 SELECT e.pat_id, 
             e.pat_enc_csn_id, 
@@ -37,40 +35,38 @@ SELECT e.pat_id,
             e.visit_prov_id,
             e.appt_status_c, 
             e.pcp_prov_id,
-            hsp.disch_disp_c, 
-            dd.name disposition, 
-            hsp.ed_disposition_c, 
-            edd.name ed_disposition,
+            --hsp.disch_disp_c, 
+            --dd.name disposition, 
+            --hsp.ed_disposition_c, 
+            --edd.name ed_disposition,
             dep.department_name,
-            dep.specialty,
-            loc.loc_name
+            dep.specialty
+            --loc.loc_name
         FROM clarity.pat_enc e
-        JOIN XDR_WHERRY_preg_pat pat on e.pat_id = pat.pat_id AND PAT.MOM_CHILD_YN ='C'			--mother encounters were pulled on step 1
-        LEFT JOIN clarity.clarity_fc fc ON e.visit_fc = fc.financial_class
-        JOIN clarity.ZC_DISP_ENC_TYPE enctype ON e.enc_type_c = enctype.disp_enc_type_c
-        
-        LEFT JOIN clarity.pat_enc_hsp       hsp ON e.PAT_ENC_CSN_ID = hsp.PAT_ENC_CSN_ID
-        LEFT JOIN clarity.zc_disch_disp     dd  ON hsp.disch_disp_c = dd.disch_disp_c
-        LEFT JOIN clarity.zc_ed_disposition edd ON hsp.ed_disposition_c = edd.ed_disposition_c
-        LEFT JOIN clarity.clarity_dep       dep ON e.department_id = dep.department_id
-        left join clarity_loc               loc ON dep.rev_loc_id = loc.loc_id
-        
+        JOIN XDR_WHERRY_preg_pat pat on e.pat_id = pat.pat_id AND PAT.MOM_CHILD_MC ='C'			--mother encounters were pulled on step 1.3
+        LEFT JOIN clarity.clarity_fc 		fc 		ON e.visit_fc = fc.financial_class
+        LEFT JOIN clarity.ZC_DISP_ENC_TYPE 	enctype ON e.enc_type_c = enctype.disp_enc_type_c
+        --LEFT JOIN clarity.pat_enc_hsp       hsp 	ON e.PAT_ENC_CSN_ID = hsp.PAT_ENC_CSN_ID
+        --LEFT JOIN clarity.zc_disch_disp     dd  	ON hsp.disch_disp_c = dd.disch_disp_c
+        --LEFT JOIN clarity.zc_ed_disposition edd 	ON hsp.ed_disposition_c = edd.ed_disposition_c
+        LEFT JOIN clarity.clarity_dep       dep 	ON e.department_id = dep.department_id
+        --LEFT JOIN XDR_WHERRY_preg_childall  cld ON 
+        --left join clarity.clarity_loc               loc ON dep.rev_loc_id = loc.loc_id
         WHERE e.enc_type_c not in (2532, 2534, 40, 2514, 2505, 2506, 2512, 2507)
-        AND e.effective_date_dt >= '01/01/2006' 
-;        --1000 seconds
+			AND e.effective_date_dt BETWEEN '01/01/2006' AND '02/05/2018';
 
 --gather counts for QA       
 SELECT COUNT(*) AS TOT_COUNT, COUNT(DISTINCT PAT_ID) AS PAT_COUNT FROM XDR_WHERRY_preg_ENC;         --2,067,185	    31,715 (1/18/18)        
 SELECT EXTRACT(YEAR FROM effective_date_dt) AS YEAR, COUNT(*) AS TOT_COUNT, COUNT(DISTINCT PAT_ID) AS PAT_COUNT FROM XDR_WHERRY_preg_ENC GROUP BY EXTRACT(YEAR FROM effective_date_dt) ORDER BY YEAR;
 
 --take a look at output
-select * from XDR_WHERRY_preg_ENC;
+--select * from XDR_WHERRY_preg_ENC;
 
 --look at the details of codes being excluded from encounters pull        
-select 
+/*select 
 * from 
 clarity.ZC_DISP_ENC_TYPE enctype
-        WHERE enctype.disp_enc_type_c  in (2532, 2534, 40, 2514, 2505, 2506, 2512, 2507)     
+        WHERE enctype.disp_enc_type_c  in (2532, 2534, 40, 2514, 2505, 2506, 2512, 2507)     */
 
 
 --Add counts for QA
@@ -85,18 +81,16 @@ COMMIT;
 --------------------------------------------------------------------------------
 --	STEP 3.2: Update FIRST_ENC_DATE and LAST_ENC_DATE in the patient table (only for mothers)
 --------------------------------------------------------------------------------	  
-alter table XDR_WHERRY_preg_pat add  "FIRST_ENC_DATE" DATE;
-alter table XDR_WHERRY_preg_pat add  "LAST_ENC_DATE" DATE;
 MERGE INTO XDR_WHERRY_preg_pat pat
 using
-  (select  pat_id
-        ,MIN(effective_date_dt) AS FIRST_ENC_DATE
-		,MAX(effective_date_dt) AS LAST_ENC_DATE
+  (select  enc.pat_id
+        ,MIN(enc.effective_date_dt) AS FIRST_ENC_DATE
+		,MAX(enc.effective_date_dt) AS LAST_ENC_DATE
     from XDR_WHERRY_preg_ENC		enc
-	JOIN XDR_WHERRY_PAT				pat on enc.pat_id = pat.pat_id and pat.MOM_CHILD_YN = 'M'
-    GROUP BY pat_id) r
+	JOIN XDR_WHERRY_preg_pat		pat on enc.pat_id = pat.pat_id
+    GROUP BY enc.pat_id) r
   on (pat.pat_id = r.pat_id)
   when matched then
       update set FIRST_ENC_DATE = r.FIRST_ENC_DATE,
 				 LAST_ENC_DATE  = r.LAST_ENC_DATE;
-COMMIT;     
+COMMIT;
