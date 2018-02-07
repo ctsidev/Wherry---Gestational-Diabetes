4682d45e2069ee43f04affe237744954b14e5ec6
jsanzucla <jsanz@mednet.ucla.edu>
2018-02-07 14:18:04 -0800

    add encounter fields

--- 4-Finalize Encounters table.sql
+++ 4-Finalize Encounters table.sql
@@ -35,23 +35,22 @@ SELECT e.pat_id,
             e.visit_prov_id,
             e.appt_status_c, 
             e.pcp_prov_id,
-            --hsp.disch_disp_c, 
-            --dd.name disposition, 
-            --hsp.ed_disposition_c, 
-            --edd.name ed_disposition,
+            hsp.disch_disp_c, 
+            dd.name disposition, 
+            hsp.ed_disposition_c, 
+            edd.name ed_disposition,
             dep.department_name,
-            dep.specialty
-            --loc.loc_name
+            dep.specialty,
+            loc.loc_name
         FROM clarity.pat_enc e
         JOIN XDR_WHERRY_preg_pat pat on e.pat_id = pat.pat_id AND PAT.MOM_CHILD_MC ='C'			--mother encounters were pulled on step 1.3
         LEFT JOIN clarity.clarity_fc 		fc 		ON e.visit_fc = fc.financial_class
         LEFT JOIN clarity.ZC_DISP_ENC_TYPE 	enctype ON e.enc_type_c = enctype.disp_enc_type_c
-        --LEFT JOIN clarity.pat_enc_hsp       hsp 	ON e.PAT_ENC_CSN_ID = hsp.PAT_ENC_CSN_ID
-        --LEFT JOIN clarity.zc_disch_disp     dd  	ON hsp.disch_disp_c = dd.disch_disp_c
-        --LEFT JOIN clarity.zc_ed_disposition edd 	ON hsp.ed_disposition_c = edd.ed_disposition_c
+        LEFT JOIN clarity.pat_enc_hsp       hsp 	ON e.PAT_ENC_CSN_ID = hsp.PAT_ENC_CSN_ID
+        LEFT JOIN clarity.zc_disch_disp     dd  	ON hsp.disch_disp_c = dd.disch_disp_c
+        LEFT JOIN clarity.zc_ed_disposition edd 	ON hsp.ed_disposition_c = edd.ed_disposition_c
         LEFT JOIN clarity.clarity_dep       dep 	ON e.department_id = dep.department_id
-        --LEFT JOIN XDR_WHERRY_preg_childall  cld ON 
-        --left join clarity.clarity_loc               loc ON dep.rev_loc_id = loc.loc_id
+        left join clarity.clarity_loc               loc ON dep.rev_loc_id = loc.loc_id
         WHERE e.enc_type_c not in (2532, 2534, 40, 2514, 2505, 2506, 2512, 2507)
 			AND e.effective_date_dt BETWEEN '01/01/2006' AND '02/05/2018';
 
