599ee34f07a1e30277c6df13f5a9eb115f801c31
jsanzucla <jsanz@mednet.ucla.edu>
2018-08-14 09:35:28 -0700

    step 8 rename step 9

---
+++ 9-Clean up.sql
@@ -0,0 +1,38 @@
+-- ******************************************************************************************************* 
+-- STEP 9
+--   Drop all the tables created during this process
+-- *******************************************************************************************************
+drop table XDR_WHERRY_PREG_ALG purge;
+drop table XDR_WHERRY_PREG_CHILDALL purge;
+drop table XDR_WHERRY_PREG_COH purge;
+drop table XDR_WHERRY_PREG_COUNTS purge;
+drop table XDR_WHERRY_PREG_DELIV purge;
+drop table XDR_WHERRY_PREG_DIST purge;
+drop table XDR_WHERRY_PREG_DX purge;
+drop table XDR_WHERRY_PREG_ENC purge;
+drop table XDR_WHERRY_PREG_ENC_DOB purge;
+drop table XDR_WHERRY_PREG_ENC_DOB_DIST purge;
+drop table XDR_WHERRY_PREG_ENC_PREGALL purge;
+drop table XDR_WHERRY_PREG_FAM purge;
+drop table XDR_WHERRY_PREG_LAB purge;
+drop table XDR_WHERRY_PREG_LABALL purge;
+drop table XDR_WHERRY_PREG_LABDRV purge;
+drop table XDR_WHERRY_PREG_MATCHING purge;
+drop table XDR_WHERRY_PREG_MATCHING_EX purge;
+drop table XDR_WHERRY_PREG_MATCHING_FINAL purge;
+drop table XDR_WHERRY_PREG_MED purge;
+drop table XDR_WHERRY_PREG_MTCHING_VECTOR purge;
+drop table XDR_WHERRY_PREG_PAT_ALLRACE purge;
+drop table XDR_WHERRY_PREG_PAT_RACE purge;
+drop table XDR_WHERRY_PREG_PL purge;
+drop table XDR_WHERRY_PREG_PLDX purge;
+drop table XDR_WHERRY_PREG_PRC purge;
+drop table XDR_WHERRY_PREG_PROV purge;
+drop table XDR_WHERRY_PREG_SOC purge;
+
+
+
+--These table contain the study_id and encounter_id and it must be deleted with caution
+--drop table XDR_WHERRY_PREG_PAT purge;
+--drop table XDR_WHERRY_PREG_ENCKEY purge;
+
